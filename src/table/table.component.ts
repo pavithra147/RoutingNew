c1ac1908fa9fad603a6ebee15d4b8846a1397774
pavithra147 <pavithra.jayavel@aspiresys.com>
2023-03-07 18:04:43 +0530

    Implemented code for search filter in individual field and implemented sorting

--- src/table/table.component.ts
+++ src/table/table.component.ts
@@ -26,7 +26,7 @@ export class TableComponent implements OnInit {
   public source!:any;
   public user:any;
   public age:any;
- 
+  public show=false;
   constructor(private sharedService: SharedService) {}
 
   ngOnInit() {
@@ -34,20 +34,19 @@ export class TableComponent implements OnInit {
    
   }
  
-  filtering(data:any){
-    this.user=this.body.filter((el:{age:number})=>{
-      return el.age == data.value.filter;
-      
-    })
-    console.log(this.user)
+  filtering(data:any , title:any){
+     title=title.toLowerCase();
+    this.user=this.body.filter((el)=>{
+      title=title;
+       return el[title] == data.value.filter;
+      }) 
     if(this.user.length==0){
       return this.body=this.source;
     }
    
     else{
-      return this.body=this.user
+      return this.body=this.user;
     }
-    
   }
   
   sources() {
@@ -70,13 +69,14 @@ export class TableComponent implements OnInit {
   }
 public item=false;
   sort(title:any){
+    title=title.toLowerCase();
     this.sharedService.getDetails().subscribe((a:any)=>{
     this.source=a;
-    if(title==="Name"){
+    
     if(this.item==true){
     this.body=this.source.sort((a:any,b:any)=>{
-       const nameA = a.name; 
-  const nameB = b.name;
+       const nameA = a[title]; 
+  const nameB = b[title];
         
         if(nameA > nameB){
           return -1;
@@ -87,8 +87,8 @@ public item=false;
     }
     else{
       this.body=this.source.sort((a:any,b:any)=>{
-        const nameA = a.name; 
-   const nameB = b.name;
+        const nameA = a[title]; 
+   const nameB = b[title];
         if(nameA < nameB){
           return -1;
         }
@@ -96,115 +96,13 @@ public item=false;
          return 0;
        })
        this.item=true;
-    }
+   }
+    })
   }
-  else if(title==="Age"){
-    if(this.item==true){
-      this.body=this.source.sort((a:any,b:any)=>{
-         const nameA = a.age; 
-    const nameB = b.age;
-          
-          if(nameA > nameB){
-            return -1;
-          }
-          return 0;
-        })
-        this.item=false;
-      }
-      else{
-        this.body=this.source.sort((a:any,b:any)=>{
-          const nameA = a.age; 
-     const nameB = b.age;
-         
-           if(nameA < nameB){
-             return -1;
-           }
-           return 0;
-         })
-         this.item=true;
-      }
-  }
-  else if(title==="Address"){
-    if(this.item==true){
-      this.body=this.source.sort((a:any,b:any)=>{
-         const nameA = a.address; 
-    const nameB = b.address;
-          
-          if(nameA > nameB){
-            return -1;
-          }
-          return 0;
-        })
-        this.item=false;
-      }
-      else{
-        this.body=this.source.sort((a:any,b:any)=>{
-          const nameA = a.address; 
-     const nameB = b.address;
-         
-           if(nameA < nameB){
-             return -1;
-           }
-           return 0;
-         })
-         this.item=true;
-      }
-  }
-  else if(title="PhoneNo"){
-    if(this.item==true){
-      this.body=this.source.sort((a:any,b:any)=>{
-         const nameA = a.phoneNo; 
-    const nameB = b.phoneNo;
-          
-          if(nameA > nameB){
-            return -1;
-          }
-          return 0;
-        })
-        this.item=false;
-      }
-      else{
-        this.body=this.source.sort((a:any,b:any)=>{
-          const nameA = a.phoneNo; 
-     const nameB = b.phoneNo;
-         
-           if(nameA < nameB){
-             return -1;
-           }
-           return 0;
-         })
-         this.item=true;
-      }
-  }
-  else if(title==="Location"){
-    if(this.item==true){
-      this.body=this.source.sort((a:any,b:any)=>{
-         const nameA = a.location; 
-    const nameB = b.location;
-          
-          if(nameA > nameB){
-            return -1;
-          }
-          return 0;
-        })
-        this.item=false;
-      }
-      else{
-        this.body=this.source.sort((a:any,b:any)=>{
-          const nameA = a.location.toUpperCase(); 
-     const nameB = b.location.toUpperCase();
-         
-           if(nameA < nameB){
-             return -1;
-           }
-           return 0;
-         })
-         this.item=true;
-      }
-  }
-   })
+  toggle(){
+    this.show=!this.show;
   }
 
-  }
+}
 
 
