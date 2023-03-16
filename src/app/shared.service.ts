37ebfe69e3f2f833776e53865e920c425006cca7
pavithra147 <pavithra.jayavel@aspiresys.com>
2023-03-16 17:59:29 +0530

    Environment variable

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -2,14 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { DeclarationListEmitMode } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from './../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SharedService {
+  public baseUrl=environment.apiUrl;   
   private subject: BehaviorSubject<string>;
   public obs$: Observable<any>;
   private sub: BehaviorSubject<string>;
+
   public obser$: Observable<any>;
   public formData: any = {};
   public array: any = [];
@@ -34,31 +37,35 @@ export class SharedService {
     return this.array;
   }
   edit(data: any) {
+   console.log(`${this.baseUrl}`);
+   
+    
     this.index = data;
   }
   delete(item: any) {
-    return this.http.delete(`http://localhost:3000/details/${item}`);
+    
+    return this.http.delete(`${this.baseUrl}/details/${item}`);
   }
   postDetails(form: any) {
-    return this.http.post('http://localhost:3000/details', form);
+    return this.http.post(`${this.baseUrl}/details`, form);
   }
   getDetails() {
-    return this.http.get('http://localhost:3000/details');
+    return this.http.get(`${this.baseUrl}/details`);
   }
   detailsToEdit(val: any) {
-    return this.http.get('http://localhost:3000/details', val);
+    return this.http.get(`${this.baseUrl}/details`, val);
   }
   put(id: any, value: any) {
-    return this.http.put(`http://localhost:3000/details/${id}`, value);
+    return this.http.put(`${this.baseUrl}/details/${id}`, value);
   }
   getdynamic(){
-    return this.http.get("http://localhost:3000/inputval");
+    return this.http.get(`${this.baseUrl}/inputval`);
   }
 
   signUpDetails(values:any){
-    return this.http.post("http://localhost:3000/register",values);
+    return this.http.post(`${this.baseUrl}/register`,values);
   }
   getSignUpDetails(){
-    return this.http.get("http://localhost:3000/register");
+    return this.http.get(`${this.baseUrl}/register`);
   }
 }
